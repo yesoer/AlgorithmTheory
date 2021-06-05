80a58b26651c7f6cccca6b018b3e67d454d77943
yesoer <luisenglaender@web.de>
2021-06-05 22:55:34 +0200

    implement huffman encoding

--- Greedy/Greedy.py
+++ Greedy/Greedy.py
@@ -65,3 +65,51 @@ def interval_lateness(intervals: list, check=False) -> list:
     
     return timeline
 
+test_frequencies = [('m', 2/11), ('i', 4/11), ('s', 4/11), ('p', 1/11)]
+
+def huffman_wrapper(frequencies: list) -> dict:
+    frequencies.sort(key = lambda x: x[1]) # sort by frequency
+    frequencies_d = {f[0]:"" for f in frequencies} # key:value , char:empty string
+    return huffman_code(frequencies, frequencies_d)
+
+# O(?), I suppose O(n) actually
+def huffman_code(frequencies_l: list, frequencies_d) -> dict:
+    # recursion anker, if frequency is one == all characters and frequencies are merged
+    if len(frequencies_l) == 1:
+        return frequencies_d
+
+    # elements to be merged are those with the smallest frequency
+    key_smallest = frequencies_l[0][0]
+    key_second_smalles = frequencies_l[1][0]
+
+    # simulate the hirarchy in the tree so add a leading 
+    # 1 or 0 to all the children in the corresponding subtree
+    for key in key_smallest:
+        frequencies_d[key] = "1" + frequencies_d[key]
+    
+    for key in key_second_smalles:
+        frequencies_d[key] = "0" + frequencies_d[key]
+    
+    # merge elements with smallest frequency
+    merged_freq = frequencies_l[0][1] + frequencies_l[1][1]
+    merged_key = key_smallest + key_second_smalles
+    merged = (merged_key, merged_freq)
+
+    # insert the new element in the list and remove the old ones
+    new_frequencies = insert_sort(frequencies_l[2:], merged, 1)
+
+    return huffman_code(new_frequencies, frequencies_d)
+
+# O(n)
+def insert_sort(l: list, elem, key=None):
+    index = 0
+    for i in range(len(l)): # find insert pos
+        list_i_val = l[i] if not key else l[i][key]
+        elem_val = elem if not key else elem[key]
+
+        if list_i_val > elem_val:
+            index = i
+            break
+      
+    l = l[:index] + [elem] + l[index:] # insert
+    return l
