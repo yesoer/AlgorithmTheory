0211eac3d23443b45d8951ef665f5b6ed73a5cfc
yesoer <luisenglaender@web.de>
2021-09-26 15:16:46 +0200

    implement prettier interval printing

--- src/Greedy/Greedy.py
+++ src/Greedy/Greedy.py
@@ -4,10 +4,41 @@
 
 test_intervals = [(1, 3), (0, 2), (2, 5), (3, 6), (4, 6)]
 
-# O(n log n)
+def print_intervals(intervals):
+    """Print intervals more or less pretty"""
 
+    column_name = "interval"
+    margin_left = len(column_name)
+    
+    # find the maximum space needed to the left
+    for i in intervals:
+        margin_left = max(len(str(i)), margin_left)
+
+    print(column_name)
+    for i in intervals:
+        # get the interval plus spacing until the timeline starts (1st column)
+        curr_line = str(i)
+        curr_line += ''.join([" "] * (margin_left - len(str(i))))
+
+        # interval on the timline (2nd column)
+        # IMPORTANT : two characters for each timestep !
+        curr_line += ''.join(["  "] * i[0])              # before start
+        curr_line += ''.join(["--"] * (i[1] - i[0])) + "-" # start to end
+
+        print(curr_line) # removing last '-'
+    
+    # leave space for (1st column)
+    max_end_time = max(intervals, key=lambda x: x[1])[1] + 1
+    last_line = ''.join([" "] * margin_left) 
+    
+    # x axis description (2nd column)
+    last_line += ''.join([str(x) + " " for x in range(max_end_time)]) 
+    last_line += "   time steps"
+    
+    print(last_line)
 
 def interval_scheduling(intervals: list) -> list:
+    """Interval Scheduling in O(n log n)"""
     intervals.sort(key=lambda x: x[1])  # sort by endtime
     timeline = []
 
