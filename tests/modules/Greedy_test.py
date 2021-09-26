984197eb1ced0c238ba6302478881d167b1cb55b
yesoer <luisenglaender@web.de>
2021-09-26 14:07:08 +0200

    implement first tests
    for
    interval partitioning
    interval_scheduling

--- tests/modules/Greedy_test.py
+++ tests/modules/Greedy_test.py
@@ -8,11 +8,23 @@ class Greedy_test(unittest.TestCase):
     def setUp(self):
         ...
 
-    def test_run(self):
-        ...
+    def test_interval_scheduling(self):
+        # a case is a tuple (input, expected result)
+        cases = [([(1, 3), (0, 2), (2, 5), (3, 6), (4, 6)], [(0,2), (2,5)])]
+        
+        for _input, expected in cases:
+            print("\n\ntesting input : ", _input, "\nwith expected output : ", expected, "\n") 
+            output = Greedy.interval_scheduling(_input)
+            self.assertEquals(output, expected)
 
-    def test_attack(self):
-        ...
+    def test_interval_partitioning(self):
+         # a case is a tuple (input, expected result)
+        cases = [([(1, 3), (0, 2), (2, 5), (3, 6), (4, 6)], [[(0, 2), (2, 5)], [(1, 3), (3, 6)], [(2, 5)], [(3, 6)], [(4, 6)]])]
+        
+        for _input, expected in cases:
+            print("\n\ntesting input : ", _input, "\nwith expected output : ", expected, "\n") 
+            output = Greedy.interval_partitioning(_input)
+            self.assertEquals(output, expected)
 
     def tearDown(self):
         ...
