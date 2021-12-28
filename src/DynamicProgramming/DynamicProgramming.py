73063d24695ba06b9647356685ff2e8fbc098b5f
yesoer <luisenglaender@web.de>
2021-12-28 20:43:01 +0100

    add docstring

--- src/DynamicProgramming/DynamicProgramming.py
+++ src/DynamicProgramming/DynamicProgramming.py
@@ -1,13 +1,22 @@
 test_weighted_edges = [(1, 2, 1), (1, 4, 0), (2, 5, 2),
                        (3, 4, 1), (3, 5, 2), (4, 5, 2), (4, 6, 2)]
 
-# find the edge (as index) that ends latest but before given edge (by index) starts
-# TODO : I think we can reduce the indexes from to (l and r) to one (check
-# cases)
-
-
-def binary_search(l: int, r: int, original_e: tuple, edges: list) -> int:
-    if r > l:
+def binary_search(l: int, r: int, original_i: tuple, intervals: list) -> int:
+    """
+    Find the interval (as index) that ends latest but before given interval
+    original_i starts in O(log n) where n is the difference r - l
+        Parameters:
+            l(int): the leftmost index to consider
+            r(int): the rightmost index to consider
+            original_i(tuple): an interval as (start, end, weight)
+            intervals(list): a list of intervals as (start, end, weight)
+            
+        Returns:
+            binary_search(int): index of the latest interval that doesn't overlap 
+                                with original_i, or None if no interval ends before
+                                original_i starts
+    """
+    if r >= l:
         mid = l + (r - l) // 2
 
         if intervals[mid][1] == original_i[0]:  # interval ends when original starts
